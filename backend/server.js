2b1522222342ab429715eabe5b2032b0034fef20
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-17 20:11:43 +0300

    Established connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-import { configDotenv } from 'dotenv';
-import express from 'express';
-import { authRoutes } from './routes/auth.route.js';
+import { configDotenv } from "dotenv";
+import express from "express";
+import { authRoutes } from "./routes/auth.route.js";
+import { connectToMongoDB } from "./db/db.js";
 configDotenv();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectToMongoDB();
 });
