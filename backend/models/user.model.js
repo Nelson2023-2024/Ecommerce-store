68319f65541c9570de4f34d2e83ad49828038745
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-17 21:57:24 +0300

    Added middleware in the user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,40 +1,61 @@
 import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    password:{
-        type: String,
-        required: true,
-        minlength: 6
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
     },
-    cartItems:[
-        {
-            quantity:{
-                type: Number,
-                default:1
-            },
-            product:{
-                type : mongoose.Schema.Types.ObjectId,
-                ref: "Product"
-            }
-        }
+    cartItems: [
+      {
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+      },
     ],
-    role:{
-        type: String,
-        enum: ["customer", "admin"],
-        default: "customer"
-    }
-},{timestamps: true});
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  { timestamps: true }
+);
 
+//before we save our user we run this function
+userSchema.pre("save", async (next) => {
+  // if password hasn't been changed call next
+  if (!this.isModified("password")) return next();
 
-export const User = mongoose.model("User", userSchema)
+  try {
+    const salt = await bcryptjs.genSalt(10);
+    this.password = await bcryptjs.hash(this.password, salt);
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async (password) => {
+  return bcryptjs.compare(password, this.password);
+};
+export const User = mongoose.model("User", userSchema);
