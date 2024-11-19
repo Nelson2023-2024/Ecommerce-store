b26ad4bc4c38df694a65b3123cfbec1fb99a4362
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-19 22:53:24 +0300

    Added getallproducts for the adminroute

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
+import { Product } from "../models/product.model.js";
 
-const router = Router()
+const router = Router();
 
-router.get('/', async (req,res) => { 
-})
+//getallproducts
+router.get("/", async (req, res) => {
+  try {
+    //get all products
+    const products = Product.find({});
+    res.status(200).json({ products });
+  } catch (error) {
+    console.log("Error in getallproducts route", error.message);
+    res.status(500).json({ error: "Server error", error: error.message });
+  }
+});
 
-export {router as productRoutes}
+export { router as productRoutes };
