efc39d4ef9531d6131232ef2d259a0ce4205a1b9
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-21 17:59:34 +0300

    Update cart quantity completed

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -54,6 +54,35 @@ router.delete("/", async (req, res) => {
 });
 
 //update quantity
-router.put("/", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  try {
+    const { id: productId } = req.params;
+    const { quantity } = req.body;
+    const user = req.user;
+
+    //check if the item exists
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+
+    if (existingItem) {
+      if (quantity === 0) {
+        //if decrement reaches 0 delete the product from the cartItems
+        user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+
+        await user.save();
+
+        return res.status(200).json(user.cartItems);
+      }else{
+        existingItem.quantity = quantity
+        await user.save()
+        res.status(200).json(user.cartItems)
+      }
+
+    }
+    else res.status(404).json({message: "Product not found"})
+  } catch (error) {
+    console.log(`Error in update quantity route`, error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+});
 
 export { router as cartRoutes };
