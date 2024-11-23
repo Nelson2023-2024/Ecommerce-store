ef36801191e9b5244908a5439b1c1cbb94e149f0
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-23 13:00:32 +0300

    Completed the coupon model and set up it's route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import { authRoutes } from "./routes/auth.route.js";
 import { productRoutes } from "./routes/product.route.js";
 import { cartRoutes } from "./routes/cart.route.js";
+import { couponRoutes } from "./routes/coupon.route.js";
 import { connectToMongoDB } from "./db/db.js";
 import cookieParser from "cookie-parser";
 configDotenv();
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
