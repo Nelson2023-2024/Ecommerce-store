104de057878c3a17880ea38bebf36bc4b2511b6f
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-24 10:06:29 +0300

    Validate coupon route completed

--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -20,4 +20,40 @@ router.get("/", async (req, res) => {
   }
 });
 
+//validate coupon
+router.get("/validate-coupon", async (req, res) => {
+  try {
+    const { code } = req.body;
+
+    //check if the code matched for the user and it is active
+    const coupon = await Coupon.findOne({
+      code: code,
+      userId: req.user._id,
+      isActive: true,
+    });
+
+    if (!coupon) return res.status(404).json({ message: "Coupon not found" });
+
+    //if the coupon has expired
+    if (coupon.expirationDate < new Date()) {
+      coupon.isActive = false;
+      await coupon.save();
+
+      return res.status(400).json({ message: "Coupon expired" });
+    }
+
+    //if user passes alll those checks
+    res
+      .status(200)
+      .json({
+        message: "Coupon is Valid",
+        code: coupon.code,
+        discountPercentage: coupon.discountPercentage,
+      });
+  } catch (error) {
+    console.log("Error in the validate coupon route", error.message);
+    res.status(500).json({ error: "Server Error", message: error.message });
+  }
+});
+
 export { router as couponRoutes };
